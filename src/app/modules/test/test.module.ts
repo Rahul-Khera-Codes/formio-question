73becd2fce653aa32dc5f53c1ef17c876ea6e81a
Prateek Srivastav <prateek29sri@gmail.com>
2020-03-21 20:31:19 +0530

    Registering formio field in multiple modules

--- src/app/modules/test/test.module.ts
+++ src/app/modules/test/test.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TestRoutingModule } from './test-routing.module';
 import { TestComponent } from './test.component';
 import { FormsModule } from '@angular/forms';
 import { FormioModule } from 'angular-formio';
+import { registerRatingComponent } from 'src/app/rating-wrapper/rating-wrapper.formio';
 
 
 @NgModule({
@@ -16,4 +17,8 @@ import { FormioModule } from 'angular-formio';
     FormioModule
   ]
 })
-export class TestModule { }
+export class TestModule {
+  constructor(private injector: Injector) {
+    registerRatingComponent(injector);
+  }
+}
